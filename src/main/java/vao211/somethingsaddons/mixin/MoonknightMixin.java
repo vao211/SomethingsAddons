dd30a53dfb08821b15d5617758d893322aa206d6
vao211 <vaolu211@gmail.com>
2026-05-25 10:04:47 +0700

    chore

--- src/main/java/vao211/somethingsaddons/mixin/MoonknightMixin.java
+++ src/main/java/vao211/somethingsaddons/mixin/MoonknightMixin.java
@@ -1,6 +1,8 @@
 package vao211.somethingsaddons.mixin;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.registry.tag.DamageTypeTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,8 +18,14 @@ import vao211.somethingsaddons.config.SomethingsAddonsConfig;
 public abstract class MoonknightMixin {
 
     @ModifyVariable(method = {"damage", "method_5643"}, at = @At("HEAD"), argsOnly = true, ordinal = 0, remap = false)
-    private float somethingsaddons$capMoonknightRawDamage(float amount) {
+    private float somethingsaddons$capMoonknightRawDamage(float amount, DamageSource source) {
+
+        if (source.isIn(DamageTypeTags.BYPASSES_INVULNERABILITY) || amount > 1000000.0f) {
+            return amount;
+        }
+
         LivingEntity entity = (LivingEntity) (Object) this;
+
         if (!SomethingsAddonsConfig.applyDmgGating) return amount;
         if (entity.getMaxHealth() < SomethingsAddonsConfig.hpToApplyDmgGating) return amount;
 
