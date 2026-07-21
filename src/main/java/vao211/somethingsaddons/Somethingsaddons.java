7d581b196537c5dc7b98932b8c9a8cba75a11c00
vao211 <vaolu211@gmail.com>
2026-07-21 16:24:12 +0700

    Fix: change config dir (SomethingsAddons/config.json -> somethingsaddonsconfig.json)

--- src/main/java/vao211/somethingsaddons/Somethingsaddons.java
+++ src/main/java/vao211/somethingsaddons/Somethingsaddons.java
@@ -2,20 +2,15 @@ package vao211.somethingsaddons;
 
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry; // Đừng quên import cái này
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import vao211.somethingsaddons.config.SomethingsAddonsConfig;
 import vao211.somethingsaddons.entity.ModEntities;
 import vao211.somethingsaddons.network.PickupLockPayload;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Somethingsaddons implements ModInitializer {
     public static final String MOD_ID = "somethingsaddons";
@@ -50,17 +45,5 @@ public class Somethingsaddons implements ModInitializer {
                 sender.sendPacket(new PickupLockPayload(currentLockedState));
             });
         });
-
-
-        Path configDir = FabricLoader.getInstance().getConfigDir().resolve("SomethingsAddons");
-        try {
-            if (!Files.exists(configDir)) {
-                Files.createDirectories(configDir);
-            }
-        } catch (IOException e) {
-            System.err.println("[SomethingsAddons] Không thể tạo thư mục config: " + e.getMessage());
-        }
-        LOGGER.info("Initializing Somethings Addons");
-        SomethingsAddonsConfig.init("SomethingsAddons/config", SomethingsAddonsConfig.class);
     }
 }
